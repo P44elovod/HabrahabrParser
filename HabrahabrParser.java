236981460e2fb0955cf495b39ff71b2217d6da20
P44elovod <eug.kovtun@gmail.com>
2017-04-26 23:21:38 +0300

    1

---
+++ HabrahabrParser.java
@@ -0,0 +1,48 @@
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.IntStream;
+
+
+public class HabrahabrParser {
+
+    public static void main(String[] args) {
+        try {
+            ArrayList<String> list = new ArrayList<>();
+            for (int count = 1; count < 4; count++) {
+                String url = "https://m.habrahabr.ru/page" + count + "/";
+                String[] title = Jsoup.connect(url)
+                        .get()
+                        .select("h3")
+                        .toString()
+                        .split("</h3>");
+
+
+
+
+                for (int index = 0; index < title.length; index++) {
+                    int cutPoint = title[index].lastIndexOf(">") + 1;
+                    title[index] = title[index].substring(cutPoint);
+                    if (title[index].length() >= 30) {
+                        list.add(title[index]);
+                    }
+                }
+
+
+            }
+
+            list
+                    .stream()
+                    .sorted()
+                    .forEach(System.out::println);
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
